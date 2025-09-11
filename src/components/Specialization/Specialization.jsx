340120765ce0428623262e18ae857901a3463aae
Léo Jansen <leo@leonardojansen.com>
2025-09-10 22:55:17 -0300

    Update Specialization.jsx

--- src/components/Specialization/Specialization.jsx
+++ src/components/Specialization/Specialization.jsx
@@ -27,7 +27,7 @@ function Specialization() {
 
 
             <div className='flex flex-col w-full items-end justify-center  p-5  py-8 xl:p-8  xl:pb-10 z-20 '>
-                <div className='shadow-[1px_1px_6px_2px_rgba(10,51,40,0.324)] p-5 px-10 rounded-2xl bg-[#f5f5f5]'>
+                <div className='shadow-[1px_1px_6px_2px_rgba(10,51,40,0.1924)] border-2 border-[#cadbdbb7] p-5 px-10 rounded-2xl bg-[#f5f5f5]'>
                 {/* Título da seção */}
                 <h2 className="text-[25px] xl:text-[30px] tracking-[-0.0678em] leading-3 xl:leading-6 text-[#0F3D3B] font-light">
                     pelo Hospital                </h2>
