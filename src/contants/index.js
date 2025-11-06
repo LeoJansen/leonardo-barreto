2ff533fa6cdec968129876a4c97603d0eebe7fb8
Léo Jansen <leo@leonardojansen.com>
2025-11-05 23:03:34 -0300

    Update WhatsApp message link to new domain
    Changed the default WhatsApp message link from www.leonardobarreto.com.br to www.drleonardobarreto.med.br to reflect the updated website address.

--- src/contants/index.js
+++ src/contants/index.js
@@ -80,6 +80,6 @@ export const conditionsDetails = {
 // Dica: use apenas números com DDI + DDD + número (ex.: Brasil 55 + 11 + 91234-5678 => "5511912345678")
 export const WHATSAPP_PHONE = process.env.NEXT_PUBLIC_WHATSAPP_PHONE || '+5546999808231';
 export const WHATSAPP_MESSAGE =
-	process.env.NEXT_PUBLIC_WHATSAPP_MESSAGE || 'Olá! Gostaria de agendar uma consulta com o Dr. Leonardo Barreto. Link disponível em www.leonardobarreto.com.br';
+	process.env.NEXT_PUBLIC_WHATSAPP_MESSAGE || 'Olá! Gostaria de agendar uma consulta com o Dr. Leonardo Barreto. Link disponível em www.drleonardobarreto.med.br';
 
 export default conditionsDetails;
